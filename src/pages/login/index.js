dd4a37b553866371fb15399f1da25b754ffb9acb
wudonglin <wdl@efanghang.com>
2019-10-16 11:24:03 +0800

    注释

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -8,7 +8,10 @@ import { setLocale } from 'utils'
 import config from 'utils/config'
 
 import styles from './index.less'
+console.log(styles)
 const FormItem = Form.Item
+
+// dva:React-Router,Redux,Redux-saga
 
 @withI18n()
 @connect(({ loading }) => ({ loading }))
@@ -122,3 +125,34 @@ Login.propTypes = {
 }
 
 export default Login
+
+/**
+  antd:
+    Form.Item
+      1.hasFeedback:配合 validateStatus 属性使用，展示校验状态图标，建议只配合 Input 组件使用;默认为false;
+      2.getFieldDecorator('username', {
+        rules: [
+          {
+            required: true,
+          },
+        ],
+      })
+      解析：this.props.form.getFieldDecorator(id, options)#
+      经过 getFieldDecorator 包装的控件，表单控件会自动添加 value（或 valuePropName 指定的其他属性）
+       onChange（或 trigger 指定的其他属性），数据同步将被 Form 接管，这会导致以下结果：
+        1.你不再需要也不应该用 onChange 来做同步，但还是可以继续监听 onChange 等事件。
+        2.你不能用控件的 value defaultValue 等属性来设置表单域的值，默认值可以用 getFieldDecorator 里的 initialValue。
+        3.你不应该用 setState，可以使用 this.props.form.setFieldsValue 来动态改变表单值。
+
+      3.
+        <Input
+          onPressEnter={this.handleOk}
+          placeholder={i18n.t`Username`}
+        />
+        onPressEnter: 回车键触发事件
+        prefix:带有前缀图标的 input;
+      
+      4.validateFieldsAndScroll:与 validateFields 相似，但校验完后，如果校验不通过的菜单域不在可见范围内，则自动滚动进可见范围
+
+
+ */
